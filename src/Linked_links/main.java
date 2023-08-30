ec8aa4c9f0e9b892abb793805a69548d869f30d4
ShauryaMalhan <seemashaurya13@gmail.com>
2023-08-30 18:56:29 +0530

    Modifed ll

--- src/Linked_links/main.java
+++ src/Linked_links/main.java
@@ -2,16 +2,24 @@ package Linked_links;
 
 public class main {
     public static void main(String[] args) {
-        LL lists = new LL();
+        // LL lists = new LL();
+        // lists.insertFirst(3);
+        // lists.insertFirst(2);
+        // lists.insertFirst(8);
+        // lists.insertFirst(17);
+        // lists.insertLast(67);
+        // lists.insert(69, 3);
+        // System.out.println(lists.deleteFirst());
+        // System.out.println(lists.deletelast());
+        // System.out.println(lists.delete(1));
+        // lists.display();
+
+        DLL lists = new DLL();
         lists.insertFirst(3);
         lists.insertFirst(2);
         lists.insertFirst(8);
         lists.insertFirst(17);
-        lists.insertLast(67);
-        lists.insert(69, 3);
-        System.out.println(lists.deleteFirst());
-        System.out.println(lists.deletelast());
-        System.out.println(lists.delete(1));
+        lists.insertLast(99);
         lists.display();
     }
 }
